2e862a4bcee3b347edba5aae7cb6b0082db1d25c
Matheuspisil <ajudaserio@yahoo.com.br>
2024-08-15 21:38:41 -0300

    deploy 2.0

--- gestordecarreiras/settings.py
+++ gestordecarreiras/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY='admin1234'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=True
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS","https://renderapi-grdq.onrender.com/", "localhost 127.0.0.1").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "renderapi-grdq.onrender.com localhost 127.0.0.1").split(" ")
 
 
 
